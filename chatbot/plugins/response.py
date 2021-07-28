8788b77064e71a3f5f7e093bf4984faa3f267127
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-07-29 00:46:00 +0530

    Update response.py

--- chatbot/plugins/response.py
+++ chatbot/plugins/response.py
@@ -6,4 +6,4 @@ async def get_response(query):
             f'https://kukichatbot.herokuapp.com/kuki/chatbot?message={query}'
         ) as resp:
             answer = await resp.json()
-            return answer['response']['bot']
+            return answer['reply']['bot']
